9452fe604eecdeedbfbf65bb0f5e383405cbce5b
Toby <toby@tobymeehan.com>
2020-04-06 19:29:54 +0100

    Add HttpClientExtensions
    Added extension class for HttpClient to enable access to library

---
+++ TobyMeehan.Http/HttpClientExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace TobyMeehan.Http
+{
+    public static class HttpClientExtensions
+    {
+        public static void Init(this HttpClient client)
+        {
+            client.BaseAddress = new Uri("");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static IHttpRequest Get(this HttpClient client, string uri)
+        {
+            return new GetHttpRequest(client, uri);
+        }
+
+        public static IHttpRequest Post(this HttpClient client, string uri, object data)
+        {
+            return new PostHttpRequest(client, uri, data);
+        }
+
+        public static IHttpRequest Put(this HttpClient client, string uri, object data)
+        {
+            return new PutHttpRequest(client, uri, data);
+        }
+
+        public static IHttpRequest Delete(this HttpClient client, string uri)
+        {
+            return new DeleteHttpRequest(client, uri);
+        }
+    }
+}
