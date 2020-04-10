d14fc4713548598994a82e931f169e5b6bac9c12
Toby <toby@tobymeehan.com>
2020-04-10 23:30:17 +0100

    Add Post, Put requests accepting HttpContent
    Added requests for Post and Put which send HttpContent, rather than a generic type

--- TobyMeehan.Http/HttpClientExtensions.cs
+++ TobyMeehan.Http/HttpClientExtensions.cs
@@ -23,9 +23,19 @@ namespace TobyMeehan.Http
             return new PostHttpRequest(client, uri, data);
         }
 
+        public static IHttpRequest PostHttpContent(this HttpClient client, string uri, HttpContent content)
+        {
+            return new PostContentHttpRequest(client, uri, content);
+        }
+
         public static IHttpRequest Put(this HttpClient client, string uri, object data)
         {
             return new PutHttpRequest(client, uri, data);
+        }
+
+        public static IHttpRequest PutHttpContent(this HttpClient client, string uri, HttpContent content)
+        {
+            return new PutContentHttpRequest(client, uri, content);
         }
 
         public static IHttpRequest Delete(this HttpClient client, string uri)
---
+++ TobyMeehan.Http/PostContentHttpRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobyMeehan.Http
+{
+    public class PostContentHttpRequest : HttpRequestBase
+    {
+        internal PostContentHttpRequest(HttpClient client, string uri, HttpContent content) : base (client, uri)
+        {
+            Content = content;
+        }
+
+        public HttpContent Content { get; }
+
+        public override async Task SendAsync()
+        {
+            HttpResponseMessage response = await Client.PostAsync(Uri, Content);
+
+            await HandleResponse(response);
+        }
+    }
+}
