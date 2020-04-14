e907bcc106d71a3e17f6f919c4424e02ef5273c4
Toby <toby@tobymeehan.com>
2020-04-14 17:16:16 +0100

    Add XML comments
    Added XML comments for basic documentation

--- TobyMeehan.Http/HttpClientExtensions.cs
+++ TobyMeehan.Http/HttpClientExtensions.cs
@@ -7,31 +7,71 @@ namespace TobyMeehan.Http
 {
     public static class HttpClientExtensions
     {
+        /// <summary>
+        /// Creates a new GET request with the specified uri.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="uri">The uri the request is sent to.</param>
+        /// <returns></returns>
         public static IHttpRequest Get(this HttpClient client, string uri)
         {
             return new GetHttpRequest(client, uri);
         }
 
+        /// <summary>
+        /// Creates a new POST request with the specified uri, which automatically formats the data as JSON.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="uri">The uri the request is sent to.</param>
+        /// <param name="data">The data to send.</param>
+        /// <returns></returns>
         public static IHttpRequest Post(this HttpClient client, string uri, object data)
         {
             return new PostHttpRequest(client, uri, data);
         }
 
+        /// <summary>
+        /// Creates a new POST request with the specified uri and content.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="uri">The uri the request is sent to.</param>
+        /// <param name="content">The HTTP request content sent to the server.</param>
+        /// <returns></returns>
         public static IHttpRequest PostHttpContent(this HttpClient client, string uri, HttpContent content)
         {
             return new PostContentHttpRequest(client, uri, content);
         }
 
+        /// <summary>
+        /// Creates a new PUT request with the specified uri, which automatically formats the data as JSON.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="uri">The uri the request is sent to.</param>
+        /// <param name="data">The data to send.</param>
+        /// <returns></returns>
         public static IHttpRequest Put(this HttpClient client, string uri, object data)
         {
             return new PutHttpRequest(client, uri, data);
         }
 
+        /// <summary>
+        /// Creates a new PUT request with the specified uri and content.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="uri">The uri the request is sent to.</param>
+        /// <param name="content">The HTTP request content sent to the server.</param>
+        /// <returns></returns>
         public static IHttpRequest PutHttpContent(this HttpClient client, string uri, HttpContent content)
         {
             return new PutContentHttpRequest(client, uri, content);
         }
 
+        /// <summary>
+        /// Creates a new DELETE request with the specified uri.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="uri">The uri the request is sent to.</param>
+        /// <returns></returns>
         public static IHttpRequest Delete(this HttpClient client, string uri)
         {
             return new DeleteHttpRequest(client, uri);
--- TobyMeehan.Http/IHttpRequest.cs
+++ TobyMeehan.Http/IHttpRequest.cs
@@ -6,11 +6,47 @@ using System.Threading.Tasks;
 
 namespace TobyMeehan.Http
 {
+    /// <summary>
+    /// Interface representing an HTTP request.
+    /// </summary>
     public interface IHttpRequest
     {
+        /// <summary>
+        /// Adds a handler for a success status code to the request.
+        /// </summary>
+        /// <typeparam name="T">Type of expected result data.</typeparam>
+        /// <param name="action">Action to execute.</param>
+        /// <returns></returns>
         IHttpRequest OnOK<T>(Action<T> action);
+
+        /// <summary>
+        /// Adds a handler for a non-success status code to the request.
+        /// </summary>
+        /// <typeparam name="T">Type of expected result data.</typeparam>
+        /// <param name="action">Action to execute.</param>
+        /// <returns></returns>
         IHttpRequest OnBadRequest<T>(Action<T, HttpStatusCode, string> action);
+
+        /// <summary>
+        /// Adds a handler which will always be invoked, regardless of the response state.
+        /// </summary>
+        /// <typeparam name="T">Type of expected result data.</typeparam>
+        /// <param name="action">Action to execute.</param>
         IHttpRequest Always<T>(Action<T, HttpStatusCode> action);
+
+        /// <summary>
+        /// Adds a generic handler which will be invoked for the specified status code.
+        /// </summary>
+        /// <typeparam name="T">Type of expected result data.</typeparam>
+        /// <param name="statusCode">Status code which will invoke the handler.</param>
+        /// <param name="action">Action to execute.</param>
+        /// <returns></returns>
+        IHttpRequest On<T>(HttpStatusCode statusCode, Action<T> action);
+
+        /// <summary>
+        /// Sends the request.
+        /// </summary>
+        /// <returns></returns>
         Task SendAsync();
     }
 }
